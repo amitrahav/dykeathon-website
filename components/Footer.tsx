4969f595b3da1aa9d0b154c4a638b0b851ff48e9
amit rahav <amit.r.89@gmail.com>
2023-04-13 19:46:07 +0300

    tlv logos on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -51,90 +51,119 @@ export const FooterImpl: React.FC = () => {
         )}
       </div>
 
-      <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
+      <div className='right-footer'>
+        <div style={{ display: "flex", gap: 15}}>
+            <a
+              href='https://lgbtqcenter.org.il/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src="https://lgbtqcenter.org.il/wp-content/uploads/2019/08/lgbt_GayCenter-Logo.svg" style={{
+                    maxHeight: "50px",
+                    width: "100%",
+                    height: "auto"
+              }}/>
+            </a>
 
-        {config.mastodon && (
-          <a
-            className={styles.mastodon}
-            href={config.mastodon}
-            title={`Mastodon ${config.getMastodonHandle()}`}
-            rel='me'
-          >
-            <FaMastodon />
-          </a>
-        )}
+            <a
+              href='https://www.tel-aviv.gov.il/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src="https://www.tel-aviv.gov.il/_layouts/15/TlvSP2013PublicSite/images/header/logo-mobile.png" style={{
+                    maxHeight: "50px",
+                    height: "auto"
+              }}/>
+            </a>
+        </div>
 
-        {config.zhihu && (
-          <a
-            className={styles.zhihu}
-            href={`https://zhihu.com/people/${config.zhihu}`}
-            title={`Zhihu @${config.zhihu}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaZhihu />
-          </a>
-        )}
 
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
+        <div className={styles.social}>
+          {config.twitter && (
+            <a
+              className={styles.twitter}
+              href={`https://twitter.com/${config.twitter}`}
+              title={`Twitter @${config.twitter}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaTwitter />
+            </a>
+          )}
 
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
+          {config.mastodon && (
+            <a
+              className={styles.mastodon}
+              href={config.mastodon}
+              title={`Mastodon ${config.getMastodonHandle()}`}
+              rel='me'
+            >
+              <FaMastodon />
+            </a>
+          )}
 
-        {config.newsletter && (
-          <a
-            className={styles.newsletter}
-            href={`${config.newsletter}`}
-            title={`Newsletter ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaEnvelopeOpenText />
-          </a>
-        )}
+          {config.zhihu && (
+            <a
+              className={styles.zhihu}
+              href={`https://zhihu.com/people/${config.zhihu}`}
+              title={`Zhihu @${config.zhihu}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaZhihu />
+            </a>
+          )}
 
-        {config.youtube && (
-          <a
-            className={styles.youtube}
-            href={`https://www.youtube.com/${config.youtube}`}
-            title={`YouTube ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaYoutube />
-          </a>
-        )}
+          {config.github && (
+            <a
+              className={styles.github}
+              href={`https://github.com/${config.github}`}
+              title={`GitHub @${config.github}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub />
+            </a>
+          )}
+
+          {config.linkedin && (
+            <a
+              className={styles.linkedin}
+              href={`https://www.linkedin.com/in/${config.linkedin}`}
+              title={`LinkedIn ${config.author}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaLinkedin />
+            </a>
+          )}
+
+          {config.newsletter && (
+            <a
+              className={styles.newsletter}
+              href={`${config.newsletter}`}
+              title={`Newsletter ${config.author}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaEnvelopeOpenText />
+            </a>
+          )}
+
+          {config.youtube && (
+            <a
+              className={styles.youtube}
+              href={`https://www.youtube.com/${config.youtube}`}
+              title={`YouTube ${config.author}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaYoutube />
+            </a>
+          )}
+        </div>
       </div>
+
     </footer>
   )
 }
