b23e0a6f21adb268be77da1951bab4e8a298a8be
amit rahav <amit.r.89@gmail.com>
2024-04-09 13:25:29 +0300

    default next public donaim

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const imgs = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
-      process.env.NEXT_PUBLIC_DOMAIN,
+      process.env.NEXT_PUBLIC_DOMAIN || "",
       'www.notion.so',
       'notion.so',
       'images.unsplash.com',
