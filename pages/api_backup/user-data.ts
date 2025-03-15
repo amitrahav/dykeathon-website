590f6261827bb135311d3e67a4ca64b47e6eaa3b
Amit rahav <amit.r.89@gmail.com>
2025-03-15 20:50:08 +0200

    throw not res

--- pages/api_backup/user-data.ts
+++ pages/api_backup/user-data.ts
@@ -1,18 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next'
 import { Client } from '@notionhq/client'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const notion = new Client({
   auth: process.env.NOTION_API_KEY
 })
 
 const queryDb = async (id: string) => {
-  if(!process.env.NOTION_API_KEY){
-    return res.status(500).send({ error: 'NOTION_API_KEY is not set' })
+  if (!process.env.NOTION_API_KEY) {
+    throw new Error('NOTION_API_KEY is not set')
   }
   const response = await notion.pages.retrieve({
     page_id: id
   })
-  return response;
+  return response
 }
 
 export default async (
@@ -22,12 +22,13 @@ export default async (
   if (req.method !== 'GET') {
     return res.status(405).send({ error: 'method not allowed' })
   }
-    const userId: string = req.query.id as string;
-    console.log({userId});
-    if (!userId) {
-        return res.status(422).send({ error: 'no userId provided' })
-    }
+  const userId: string = req.query.id as string
+  console.log({ userId })
+  if (!userId) {
+    return res.status(422).send({ error: 'no userId provided' })
+  }
 
+  try {
     const notionRes = await queryDb(userId)
 
     if (!notionRes) {
@@ -36,4 +37,9 @@ export default async (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     res.status(200).json(notionRes.properties)
+  } catch (error) {
+    return res.status(500).send({
+      error: error instanceof Error ? error.message : 'An error occurred'
+    })
+  }
 }
