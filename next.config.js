cf62fb23d09f792c0430a24a38863c2a38255930
Amit rahav <amit.r.89@gmail.com>
2025-03-22 14:41:56 +0200

    remove localhost

--- next.config.js
+++ next.config.js
@@ -14,8 +14,7 @@ const imgs = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it',
-      'localhost'
+      'transitivebullsh.it'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
